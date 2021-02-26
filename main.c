8921c747a9753dddf81bcba2beeae68eee42b803
Yudy Valentino <13517128@std.stei.itb.ac.id>
2021-02-26 22:21:33 +0700

    define event type

--- main.c
+++ main.c
@@ -2,25 +2,50 @@
 #include "simlib.h" /* Required for use of simlib.c. */
 
 
-// DEFINE STATIC VALUES
-// NEED CROSS CHECK
-#define EVENT_ARRIVAL_PERSON_TERMINAL_1   1
-#define EVENT_ARRIVAL_PERSON_TERMINAL_2   2
-#define EVENT_ARRIVAL_PERSON_CAR_RENTAL   3
+/** 
+ * DEFINE EVENT TYPE ASSIGNMENT
+*/
+/**
+ * Event type for arrival
+*/
+#define EVENT_ARRIVAL_PERSON_TERMINAL_1   1 
+#define EVENT_ARRIVAL_PERSON_TERMINAL_2   2 
+#define EVENT_ARRIVAL_PERSON_CAR_RENTAL   3 
+
+//
 #define EVENT_ARRIVAL_BUS_TERMINAL_1      4
 #define EVENT_ARRIVAL_BUS_TERMINAL_2      5
 #define EVENT_ARRIVAL_BUS_CAR_RENTAL      6
+
+/**
+ * Event type for departure
+*/
 #define EVENT_DEPARTURE_BUS_TERMINAL_1    7
 #define EVENT_DEPARTURE_BUS_TERMINAL_2    8
 #define EVENT_DEPARTURE_BUS_CAR_RENTAL    9
+
+/**
+ * Event type for unload person
+*/
 #define EVENT_UNLOAD_PERSON_TERMINAL_1    10
 #define EVENT_UNLOAD_PERSON_TERMINAL_2    11
 #define EVENT_UNLOAD_PERSON_CAR_RENTAL    12
+
+/**
+ * Event type for load person
+*/
 #define EVENT_LOAD_PERSON_TERMINAL_1      13
 #define EVENT_LOAD_PERSON_TERMINAL_2      14
 #define EVENT_LOAD_PERSON_CAR_RENTAL      15
+
+/**
+ * Event type for end the simulation
+*/
 #define EVENT_END_SIMULATION              16
 
+/**
+ * Random stream for TUBES 1
+*/
 #define STREAM_INTERARRIVAL_TERMINAL_1  1
 #define STREAM_INTERARRIVAL_TERMINAL_2  2
 #define STREAM_INTERARRIVAL_CAR_RENTAL  3
@@ -28,6 +53,9 @@
 #define STREAM_LOADING                  5
 #define STREAM_DESTINATION              6
 
+/**
+ * Define variable
+*/
 #define DESTINATION_TERMINAL_1 1
 #define DESTINATION_TERMINAL_2 2
 #define DESTINATION_CAR_RENTAL 3
@@ -39,23 +67,11 @@
 #define LIST_BUS_TO_TERMINAL_2  5
 #define LIST_BUS_TO_CAR_RENTAL  6
 
-#define DATA_ARRIVAL_TIME 1
-#define DATA_DESTINATION  2
-
-#define VARIABLE_NQ_TERMINAL_1 1
-#define VARIABLE_NQ_TERMINAL_2 2
-#define VARIABLE_NQ_CAR_RENTAL 3
-#define VARIABLE_DELAY_TERMINAL_1 4
-#define VARIABLE_DELAY_TERMINAL_2 5
-#define VARIABLE_DELAY_CAR_RENTAL 6
-#define VARIABLE_NQ_BUS 7
-#define VARIABLE_BUS_STOP_TERMINAL_1 8
-#define VARIABLE_BUS_STOP_TERMINAL_2 9
-#define VARIABLE_BUS_STOP_CAR_RENTAL 10
-#define VARIABLE_BUS_LOOP 11
-#define VARIABLE_PERSON_SYSTEM 12
-
+/**
+ * Max bus capacity
+*/
 #define MAX_BUS_SIZE 20
+
 
 
 
