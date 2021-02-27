b77bf7cdcf3c57a875e80df5415fbcceacb41b67
Yudy Valentino <13517128@std.stei.itb.ac.id>
2021-02-27 23:25:11 +0700

    add function structure

--- main.c
+++ main.c
@@ -2,6 +2,91 @@
 #include "simlib.h" /* Required for use of simlib.c. */
 #include "lib.h"
 
+
+/**
+ * Termiinal 1
+*/
+void arrivalPersonTerminal1() {
+
+}
+
+void arrvialBusTerminal1() {
+
+}
+
+void departureBusTerminal1() {
+
+}
+
+void unloadPersonTerminal1() {
+
+}
+
+void loadPersonTerminal1() {
+
+}
+
+/**
+ * Terminal 2
+*/
+void arrivalPersonTerminal2() {
+
+}
+
+void arrvialBusTerminal2() {
+
+}
+
+void departureBusTerminal2() {
+
+}
+
+void unloadPersonTerminal2() {
+
+}
+
+void loadPersonTerminal2() {
+
+}
+
+
+/**
+ * Car rental
+*/
+void arrivalPersonCarRental() {
+
+}
+
+void arrvialBusCarRental() {
+
+}
+
+void departureBusCarRental() {
+
+}
+
+void unloadPersonCarRental() {
+
+}
+
+void loadPersonCarRental() {
+
+}
+
+/**
+ * Init model simulation
+*/
+void initModel() {
+
+}
+
+/**
+ * Print answer for TUBES 1
+*/
+void print() {
+
+}
+
 int main() {
     
 }
