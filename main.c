16cbb82790b4456fb60acff800dc4b7172aa596b
Yudy Valentino <13517128@std.stei.itb.ac.id>
2021-03-01 00:41:42 +0700

    add function for terminal 1

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include "simlib.h" /* Required for use of simlib.c. */
 #include "lib.h"
 
-int numberBusCapacity() {
+double busArrival = 0.0;
+double busDepartureFromCarRental = 0.0;
+
+int listBusCapacity() {
     return list_size[LIST_BUS_TO_TERMINAL_1] + list_size[LIST_BUS_TO_TERMINAL_2] + list_size[LIST_BUS_TO_CAR_RENTAL];
 }
 
@@ -21,19 +24,50 @@ void arrivalPersonTerminal1() {
 }
 
 void arrvialBusTerminal1() {
-
+    busArrival = sim_time;
+    if (list_size[LIST_BUS_TO_TERMINAL_1] > 0) {
+        event_schedule(sim_time + uniform(16,24, STREAM_UNLOADING), EVENT_UNLOAD_PERSON_TERMINAL_1);
+    } else if (list_size[LIST_TERMINAL_1] > 0 && listBusCapacity() < MAX_BUS_SIZE) { 
+        event_schedule(sim_time + uniform(15,25,STREAM_LOADING), EVENT_LOAD_PERSON_TERMINAL_1);
+    } else {
+        event_schedule(sim_time + 5.0 * 60.0, EVENT_DEPARTURE_BUS_TERMINAL_1);
+    }
 }
 
 void departureBusTerminal1() {
-
+    if ((sim_time - busArrival) < (5.0 * 60.0)) {
+        event_schedule(busArrival + 5.0 * 60.0, EVENT_DEPARTURE_BUS_TERMINAL_1);
+    } else {
+        sampst ((sim_time - busArrival), VARIABLE_BUS_STOP_TERMINAL_1);
+        event_schedule(sim_time + 1.0/(30.0 * 60.0 * 60.0), EVENT_ARRIVAL_BUS_TERMINAL_2);
+    }
 }
 
 void unloadPersonTerminal1() {
+    list_remove(FIRST, LIST_BUS_TO_TERMINAL_1);
+    timest(listBusCapacity(), VARIABLE_NUM_BUS);
+    sampst(sim_time - transfer[DATA_ARRIVAL_TIME], VARIABLE_PERSON_SYSTEM);
 
+    if (list_size[LIST_BUS_TO_TERMINAL_1] > 0) {
+        event_schedule(sim_time + uniform(16, 24, STREAM_UNLOADING), EVENT_UNLOAD_PERSON_TERMINAL_1);
+    }else if (list_size[LIST_TERMINAL_1] > 0 && listBusCapacity() < MAX_BUS_SIZE) {
+        event_schedule(sim_time + uniform(15, 25, STREAM_LOADING ), EVENT_LOAD_PERSON_TERMINAL_1);
+    }else {
+        event_schedule(sim_time, EVENT_DEPARTURE_BUS_TERMINAL_1);
+    }
 }
 
 void loadPersonTerminal1() {
-
+    list_remove(FIRST, LIST_TERMINAL_1);
+    timest(list_size[LIST_TERMINAL_1], VARIABLE_NUM_TERMINAL_1);
+    sampst((sim_time - transfer[DATA_ARRIVAL_TIME]), VARIABLE_DELAY_TERMINAL_1);
+    list_file(LAST, LIST_BUS_TO_CAR_RENTAL);
+    timest(list_bus_size(), VARIABLE_NUM_BUS);
+    if (list_size[LIST_TERMINAL_1] > 0 && list_bus_size() < MAX_BUS_SIZE) {
+        event_schedule(sim_time + uniform(15, 25, STREAM_LOADING), EVENT_LOAD_PERSON_TERMINAL_1);
+    }else {
+        event_schedule(sim_time, EVENT_DEPARTURE_BUS_TERMINAL_1);
+    }
 }
 
 /**
